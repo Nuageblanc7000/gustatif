0e30949219926b4b4eae521e746438d8913f0d5f
remy <wetterene.remy@gmail.com>
2022-08-23 20:02:05 +0200

    mise à jour js btn smooth page restaurant

--- assets/javascript/slider.js
+++ assets/javascript/slider.js
@@ -34,20 +34,71 @@ controls.forEach((elem, c)=>{
 })
 
 
-//test
+//map
 const coord = document.querySelector('#map');
-const longi = parseFloat(coord.dataset.l).toFixed(3)
-const lati = parseFloat(coord.dataset.la).toFixed(3)
-console.log(parseFloat(longi))
-let map = L.map('map').setView([parseFloat(longi),parseFloat(lati)], 20);
 
+if(coord !== null){
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  const longi = parseFloat(coord.dataset.l).toFixed(3)
+  const lati = parseFloat(coord.dataset.la).toFixed(3)
+  console.log(parseFloat(longi))
+  let map = L.map('map').setView([parseFloat(longi),parseFloat(lati)], 20);
+  
+  
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 12,
     minZoom:12,
     attribution: '© OpenStreetMap'
-}).addTo(map);
-const logiConvert = (parseFloat(longi) - 0.00194).toFixed(3)
-const latiConvert = (parseFloat(lati) - 0.001).toFixed(2)
+  }).addTo(map);
+  const logiConvert = (parseFloat(longi) - 0.00194).toFixed(3)
+  const latiConvert = (parseFloat(lati) - 0.001).toFixed(2)
 console.log(logiConvert)
 var marker = L.marker([logiConvert , latiConvert]).addTo(map);
+}
+
+
+
+//scroll page restaurant
+
+const nav_resto_lateral = [...document.querySelectorAll('.resto-cube')]
+const nav_resto_lateral_r = [...document.querySelectorAll('.resto-cube-r')]
+const section = [...document.querySelectorAll('.js-move')] 
+const header_container = document.querySelector('.header-container')
+const bounding_header =  header_container.getBoundingClientRect()
+const {height} = bounding_header 
+
+
+let sectionPosi;
+function calculePos(){
+ sectionPosi = section.map(pos => pos.offsetTop)
+}
+
+calculePos()
+
+function addEvent(elem,ev){
+  elem.forEach(each => {
+    each.addEventListener('click',ev)
+  });
+
+}
+addEvent(nav_resto_lateral,scrollElement)
+addEvent(nav_resto_lateral_r,scrollResponsive)
+
+function scrollElement (e) {
+  const linkIndex = nav_resto_lateral.indexOf(this)
+  console.log(linkIndex)
+  window.scrollTo({
+    top:sectionPosi[linkIndex] - (height),
+    behavior:'smooth'
+  })
+
+}
+function scrollResponsive (e) {
+  console.log(height)
+  const linkIndex = nav_resto_lateral_r.indexOf(this)
+  window.scrollTo({
+    top:(sectionPosi[linkIndex] - (height)) ,
+    behavior:'smooth'
+  })
+
+}
