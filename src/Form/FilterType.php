242e58e96637503af1a5e28028017e871c47d609
remy <wetterene.remy@gmail.com>
2022-09-11 23:52:15 +0200

    je prie que ça fonctione

--- src/Form/FilterType.php
+++ src/Form/FilterType.php
@@ -2,21 +2,22 @@
 
 namespace App\Form;
 
+use App\Entity\Origine;
 use App\Data\DataFilter;
 use App\Entity\Category;
-use App\Entity\Origine;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SearchType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class FilterType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('s',SearchType::class,
+            ->add('s',TextType::class,
                 [
                     'attr' =>
                     [
