d27132204b5acd3b3309aa2b3fcea71ef364f9b1
remy <wetterene.remy@gmail.com>
2022-09-12 22:46:25 +0200

    mise à jour en cours de la page home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,13 +4,15 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Validator\Constraints\Date;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
     public function index(): Response
     {
-        
+        $date = Date('d/Y:m');
+        dd($date);
         return $this->render('home/home.html.twig', [
             'controller_name' => 'HomeController',
         ]);
