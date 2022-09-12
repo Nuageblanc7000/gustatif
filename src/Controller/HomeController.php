61c17b68e019e3ac892299e5146ec512b36844d5
remy <wetterene.remy@gmail.com>
2022-09-13 00:29:34 +0200

    mise en place du système aléatoire page home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace App\Controller;
+
+use DateTime;
+use DateTimeZone;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,13 +14,31 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
-        $date = Date('d/Y:m');
-        dd($date);
+        //date de la base de donnée!
+        $dateUpdate = '26-09-2022';
+
+        $firstday = new \DateTimeImmutable();
+        $firstday->setTimezone(new DateTimeZone('Europe/Paris'));
+        $nowTimeZone= $firstday->setTimezone(new DateTimeZone('Europe/Paris'))->format('j-m-Y');
+    
+        $dateTime = $firstday;
+        $dateFormat = $dateTime->format('j-m-y');
+
+        $monday = date('j-m-Y',strtotime($dateUpdate.'+7 days'));
+        dd($monday);
+        if($monday === $nowTimeZone){
+            dd('ok');
+        }else{
+            dd('non il reste encore '.''. date('j',strtotime($monday) - strtotime($nowTimeZone)));
+        }
+
+
         return $this->render('home/home.html.twig', [
             'controller_name' => 'HomeController',
         ]);
 
         
-    }
     
+    
+}
 }
